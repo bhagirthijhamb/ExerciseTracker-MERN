6a1907022105e255c495dd5661988eef51339715
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-03-10 10:10:13 -0500

    added bcrypt to hash the user password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,10 +1,43 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
 
 const userSchema = mongoose.Schema({
   name: { type: String, required: true, minlength: 4 },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
 }, { timeStamps: true })
+
+userSchema.pre('save', async function(next) {
+  try {
+    const user = this;
+    if(!user.isModified('password')){
+      next();
+    }
+
+    // const salt = await bcrypt.genSalt(10);
+    // const hash = await bcrypt.hash(user.password, salt);
+
+    bcrypt.genSalt(10, async function(err, salt) {
+      if(err){
+        return next(err);
+      }
+
+      bcrypt.hash(user.password, salt, null, (err, hash) => {
+        if(err){
+          return next(err);
+        }
+
+        user.password = hash;
+        next()
+      })
+    })
+
+    // user.password = hash;
+    // next();
+  } catch(error){
+    next(error)
+  }
+})
 
 const User = mongoose.model('User', userSchema);
 
