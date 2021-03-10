a58d62b9a743e77673356d8c92aa92c39750b719
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-03-10 10:43:39 -0500

    added JWT token to be returned when the user signs up

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,11 @@
+const jwt = require('jwt-simple');
 const User = require('./../models/userModel');
 const { validateSignupData } = require('./../utils/validators');
+
+const tokenForUser = (user) => {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, process.env.JWT_SECRET )
+}
 
 exports.signup = async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -16,7 +22,8 @@ exports.signup = async (req, res, next) => {
     })
 
     const newUser = await user.save();
-    res.json(newUser);
+    // res.json(newUser);
+    res.json({ token: tokenForUser(newUser) })
   } catch(error){
     console.log(error);
     next(error);
