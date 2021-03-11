1eb188cf0464497206beda018f2d42968f15c8c6
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-03-11 14:56:02 -0500

    Signup component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,16 @@
 import react from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import App from './components/App';
+import Welcome from './components/Welcome';
+import Signup from './components/auth/Signup';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    {/* <App /> */}
+    <App>
+      {/* <Welcome /> */}
+      <Route path="/" component={Welcome} />
+      <Route path="/signup" component={Signup} />
+    </App>
   </BrowserRouter>, document.querySelector('#root'));
