a39ac078261d214a7c1df50965a00e8a4594c7c6
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-03-11 09:56:22 -0500

    Get One Exercise Route added

--- backend/routes/exerciseRoutes.js
+++ backend/routes/exerciseRoutes.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = (app) => {
-  app.get('/exercises', exerciseController.getExercises);
-  app.post('/exercises/add', exerciseController.addExercise);
+  app.get('/exercises', requireAuth, exerciseController.getExercises);
+  app.post('/exercises/add', requireAuth, exerciseController.addExercise);
+  app.get('/exercises/:id', requireAuth, exerciseController.getOneExercise);
 }
