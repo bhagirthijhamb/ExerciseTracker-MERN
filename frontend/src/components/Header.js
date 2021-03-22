1fc4a55301f52dd36a466c5b67731f7062827c56
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-03-21 22:50:16 -0400

    Update the header based on Auth status

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class Header extends Component {
-  render(){
-    return(
-      // <div>
-      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link className="navbar-brand mb-0 h1" to='/'>Exercise Tracker</Link>
-
+  renderLinks () {
+    if(this.props.authenticated){
+      return (
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
             <Link className="nav-link" to='/exercises'>Exercises</Link>
@@ -16,19 +14,39 @@ class Header extends Component {
             <Link className="nav-link" to='/createExercise'>Create Exercise Log</Link>
           </li>
           <li className="nav-item">
+            <Link className="nav-link" to='/signout'>Sign Out</Link>
+          </li>
+        </ul>
+      )
+    }
+      else {
+        return (
+           <ul className="navbar-nav mr-auto">
+          
+          <li className="nav-item">
             <Link className="nav-link" to='/signup'>Sign Up</Link>
           </li>
           <li className="nav-item">
             <Link className="nav-link" to='/signin'>Sign In</Link>
           </li>
-          <li className="nav-item">
-            <Link className="nav-link" to='/signout'>Sign Out</Link>
-          </li>
         </ul>
+        )
+      }
+  }
+  render(){
+    return(
+      // <div>
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link className="navbar-brand mb-0 h1" to='/'>Exercise Tracker</Link>
+        {this.renderLinks()}
       </nav>
       // </div>
     )
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return { authenticated: state.auth.authenticated }
+}
+
+export default connect(mapStateToProps)(Header);
