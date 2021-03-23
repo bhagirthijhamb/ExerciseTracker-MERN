f8b075cb73927a2a19b33a280d760f31fef567c5
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-03-23 10:48:21 -0400

    Added code for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const cors = require('cors');
 const connectDB = require('./utils/db');
 const errorHandlingMiddleware = require('./middlewares/errorHandleMiddleware');
+
+console.log('__dirname', __dirname);
+const dirname = __dirname + '/../';
+console.log('dirname', dirname);
 
 dotenv.config();  
 connectDB();
@@ -14,8 +19,22 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
+// app.get('/', (req, res) => {
+//   res.send('API is running');
+// })
+
 require('./routes/userRoutes')(app);
 require('./routes/exerciseRoutes')(app);
+
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(dirname, '/frontend/build')))
+  app.get('*', (req, res) => res.sendFile(path.resolve(dirname, 'frontend', 'build', 'index.html')))
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  })
+}
 
 app.use(errorHandlingMiddleware.notFound);
 app.use(errorHandlingMiddleware.errorHandler);
